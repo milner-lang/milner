9b1666be158901e471bd572dcbbdb7aa65d3b1e0
Alan Hu <hu.ala@northeastern.edu>
2020-06-05 00:07:28 -0400

    Restore removed test cases

--- test/runpass/fun.ml
+++ test/runpass/fun.ml
@@ -1,2 +1,18 @@
+val foo : fun((), ()) -> ()
+fun foo(x as y, ()) = y; x
+
 val bar : fun(()) -> ()
-fun bar(x as y) = (); x; y; x
+fun bar(x as y) = x; (); x; y
+
+fun str() = "FOO BAR BAZ"
+
+fun str2() = " \" \n "
+
+val nil : fun() -> ()
+fun nil() = ()
+
+fun str_fun("foo") = "foofoo"
+  | ("bar") = "barbar"
+  | ("baz") = "bazbaz"
+  | ("") = "empty"
+  | (s) = s
