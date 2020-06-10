1697b69aa422f1abd7e1765c4aeb82d6c8105c51
Alan Hu <hu.ala@northeastern.edu>
2020-06-09 22:18:00 -0400

    Implement variable patterns

--- examples/int_match.ml
+++ examples/int_match.ml
@@ -2,10 +2,15 @@ external puts : fun(Cstring) -> ()
 
 val cases : fun(Int32) -> Cstring
 fun cases(0i32) = "Hello world!"
-       | (_) = "Goodbye world!"
+  | (_) = "Goodbye world!"
+
+fun str_cases("") = "Empty"
+  | (s) = s
 
 val main : fun() -> Int32
 fun main() =
   puts(cases(0i32));
   puts(cases(1i32));
+  puts(str_cases(""));
+  puts(str_cases("Ichi ni san"));
   0i32
